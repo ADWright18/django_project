d925ca45e2c59eb0cadf7fda260b658bf8949ac1
Adomous Wright <adomous.wright@uconn.edu>
2018-01-05 13:53:05 -0500

    Database shema update

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # /music/
+    path('', views.index, name='index'),
+
+    # /music/album/id
+    path('album/<album_id>/', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>This is the Music app homepage.</h1> Hello again!")
+
+def detail(request, album_id):
+    return HttpResponse("<h1>Details for album id:" + str(album_id) + "</h1>")
